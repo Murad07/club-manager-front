af76480937f1ea98ef5308726fdcf5e9cd564051
Murad07 <murad.pi22@gmail.com>
2021-06-29 01:49:46 +0600

    add live server url of api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://localhost:5000/`,
+  baseURL: `https://limitless-dusk-57570.herokuapp.com/`,
   // timeout: 1000,
   //   headers: { application: "application/json" },
   //   params: { api_token: "abcd" },
